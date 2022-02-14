55ceb523a580457941a81492a0279596db2eec07
Priscila <priscilapintado66@gmail.com>
2022-02-14 11:57:54 -0500

    added valid anagram

---
+++ DataStructures/HashTable/validAnagram.js
@@ -0,0 +1,51 @@
+/* 
+242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once. 
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2: 
+Input: s = "rat", t = "car"
+Output: false
+
+https://leetcode.com/problems/valid-anagram/
+*/
+
+const isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  let anagramTable = {};
+
+  for (let i = 0; i < s.length; i++) {
+    let currChar = s[i];
+    if (!anagramTable[currChar]) {
+      anagramTable[currChar] = 0;
+    }
+    anagramTable[currChar]++;
+  }
+  for (let j = 0; j < t.length; j++) {
+    let currChar2 = t[j];
+    if (!anagramTable[currChar2]) {
+      //if value of key is zero then we are about to go to -1
+      return false;
+    }
+    anagramTable[currChar2]--;
+  }
+
+  return true;
+};
+
+/* 
+Notes:
+•If the final key-value pair of char is "a" is "a":0  then strS and strS both have the same amount of "a"'s
+• anagramTable must have only value of zero by time we finish iterating through the second for loop
+    *If this is not the case, then strT is not anagram of strS
+•if (!anagramTable[currChar2]) returns false if:
+    * currChar2 doesn't exist in anagramTable
+    * currChar2 value in the anagramTable is about to be decremented to -1
+*/
---
+++ DataStructures/LRU_Cache/lru_cache.js
@@ -0,0 +1,82 @@
+class LRUCache {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.count = 0;
+    this.head = null;
+    this.tail = null;
+    this.hash = {};
+  }
+
+  get(key) {
+    if (this.hash[key]) {
+      const { value, prev, next } = this.hash[key];
+      //delete the connections of this node, get ready to move this node to head
+      if (prev) {
+        prev.next = next;
+      }
+      if (next) {
+        next.prev = prev;
+      }
+      //if the get value was the next to be removed
+      if (this.tail === this.hash[key]) {
+        this.tail = prev || this.hash[key];
+      }
+
+      //redefine prev
+      this.hash[key].prev = null;
+
+      if (this.head !== this.hash[key]) {
+        this.hash[key].next = this.head;
+        this.head.prev = this.hash[key];
+      }
+      //redefine head
+      this.head = this.hash[key];
+      return value;
+    }
+    return -1;
+  }
+
+  put(key, value) {
+    //if key already exists
+    if (this.hash[key]) {
+      this.hash[key].value = value;
+      //update most recently used by calling get function
+      this.get(key);
+    } else {
+      //create new node
+      this.hash[key] = {
+        key: key,
+        value: value,
+        prev: null,
+        next: null,
+      };
+      //if there is a head, update the bindings
+      if (this.head) {
+        this.hash[key].next = this.head;
+        this.head.prev = this.hash[key];
+      }
+      //redefine head
+      this.head = this.hash[key];
+
+      //if no tail, make new head tail
+      if (!this.tail) {
+        this.tail = this.hash[key];
+      }
+
+      this.count += 1;
+    }
+
+    if (this.count > this.capacity) {
+      let removeKey = this.tail.key;
+      //remove tail bindings
+      if (this.tail.prev) {
+        this.tail.prev.next = null;
+        this.tail = this.tail.prev;
+      }
+
+      //delete this node from hashTable
+      delete this.hash[removeKey];
+      this.count -= 1;
+    }
+  }
+}
