b9bf7f6c15e8b2d85c701c04ef81c9f0b8475588
Priscila <priscilapintado66@gmail.com>
2022-02-17 08:52:32 -0500

    added: contains duplicate

---
+++ DataStructures/Array/containsDuplicate.js
@@ -0,0 +1,33 @@
+/* 
+217. Contains Duplicate
+https://leetcode.com/problems/contains-duplicate/
+*/
+
+const containsDuplicate = function (nums) {
+  let numTable = {};
+  for (let i = 0; i < nums.length; i++) {
+    let currNum = nums[i];
+    if (currNum in numTable) {
+      return true;
+    }
+    numTable[currNum] = 0;
+  }
+  return false;
+};
+
+//main function: to find a duplicate in array
+//once we see a duplicate we can return true
+//  • char in charTable then return true
+//Input: nums = [1,2,3,1]  true
+
+//Input: nums = [1,2,3,4]  false
+
+/**
+Time: O(n)
+    -We iterate through each element in the array once (O(n) time)
+        •On average hashTable lookups take up O(1) time
+        •On average hashTable insertions take up O(1) time
+Space:
+    -At worst case our array will not have any duplicates
+        •The number of entries in hashTable will be equal to the size of the array
+ */
