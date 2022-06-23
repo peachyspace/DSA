6b5ff7c1c9255e58537e536151ce77009be260a6
Priscila <priscilapintado66@gmail.com>
2022-06-23 15:40:04 -0400

    added dfs solution

--- DataStructures/BinaryTree/binaryTreeRightSideView.js
+++ DataStructures/BinaryTree/binaryTreeRightSideView.js
@@ -3,32 +3,18 @@
 https://leetcode.com/problems/binary-tree-right-side-view/
 */
 
-const rightSideView = function (root) {
-  if (root === null) return [];
-  const queue = [];
-  queue.push(root);
-  const result = [];
-  while (queue.length) {
-    let numOfParents = queue.length;
-    result.push(queue[numOfParents - 1]);
-    while (numOfParents) {
-      let parent = queue.shift();
-      numOfParents--;
-      if (parent.left) queue.push(parent.left);
-      if (parent.right) queue.push(parent.right);
-    }
-  }
-  return result;
-};
-/* 
-Time: O(n*n)-> O(n^2), n is the total number of nodes in the tree
-• We visit every node in the tree
-	◦ Every time we visit we shift a node from the queue(array) and that takes O(n) time
-		‣ BFS: at worst case we can have a complete tree and that means that at the last level we will have n/2 nodes in the queue which essentially is n
-Space: O(n)
-• The queue will have at most n n nodes 
-	◦ BFS: at worst case we can have a complete tree and that means that at the last level we will have n/2 nodes in the queue which essentially is n
-*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
 
 /* 
 //standing on the right side, what can you see?
@@ -36,9 +22,59 @@ Space: O(n)
 //BUT you might be able to see a left node if there is no right node blocking it
 //HENCE we are looking for the right most node on each level
 //- use BFS and add the last node on every level to the result array
+//- you can also use dfs by using preOrder traversal
 
 Input: root = null
 Output: []
+*/
+
+const rightSideView = (root) => {
+  if (!root) return [];
+  const queue = [];
+  const result = [];
+  queue.push(root);
+  while (queue.length) {
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+      if (i === size - 1) {
+        result.push(node.val);
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+  return result;
+};
+
+/*
+Time: O(n*n) -> O(n^2)
+- n is the total number of nodes in the 
+  given binary tree
+- we visited every node once but we used the 
+  shift method on them
+  - shift method is an O(n) operation
+  
+Space: O(n/2) -> O(n)
+- In the worst case the tree is a full
+  binary tree meaing that the queue
+  can hold n/2 nodes at the last level
+ 
+*/
+
+/*
+If root is null return []
+Create queue
+Add root ot queue
+Iterate while the queue is not empty
+- Size = queue.length
+- Iterate for size times
+- Take out node from queue
+- If i === size-1 then add node.val to result
+- If node.left   is not null then add to queue
+- If node.right is not null then add to queue
+Return result
+
 */
 
 //Optimization using LinkedLists
@@ -100,8 +136,61 @@ const rightSideView2 = function (root) {
 Time: O(n), n is the total number of nodes in the tree
 • We visit every node in the tree
 	◦ Every time we visit we dequeue a node from the queue(array) and that takes O(1) time
-		‣ BFS: at worst case we can have a complete tree and that means that at the last level we will have n/2 nodes in the queue which essentially is n
+
 Space: O(n)
-• The queue will have at most n n nodes 
-	◦ BFS: at worst case we can have a complete tree and that means that at the last level we will have n/2 nodes in the queue which essentially is n
+• BFS: at worst case we can have a complete tree and that means that at the last level we will have n/2 nodes in the queue which essentially is n
+*/
+
+//Optimization using dfs
+const rightSideView3 = (root) => {
+  const result = [];
+  const dfs = (root, level) => {
+    if (!root) return;
+    //the last assigned value
+    //is the rightmost value
+    result[level] = root.val;
+    dfs(root.left, level + 1);
+    dfs(root.right, level + 1);
+  };
+  dfs(root, 0);
+  return result;
+};
+
+/*
+Time: O(n)
+- n is the total number of nodes in the 
+given binary tree
+- we visited every node in the given binary
+tree once
+
+
+Space: Worst: O(n), Best: O(log(n))
+- h is the height of the tree
+- the height of any node/tree is the max
+height of left or right path till the 
+leaf
+- In the best case the tree is a balanced
+binary tree meaing that the call stack
+can hold h frames. The height of a 
+balanced binary tree is O(log(n))
+- In the worst case the binary tree is
+skewed, meaning that the call stack
+can hold n frames
+*/
+
+/*
+Create result array
+dfs function(root, level)
+- if the root is null then return
+- use the level as an index in order
+to place the node.val there
+- we will overwrite the values placed
+  at that index BUT the last value
+  placed is the right most value of
+  the current level and that wont 
+  be overwritten
+- Explore the left child and level+1
+- Explore the right child and level+1
+call dfs(root,0)
+return result
 */
